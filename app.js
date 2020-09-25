4a61057ce3cdca0bc8bb9fcc60e2f807c421eeb2
Ahmed Ezzat <moreone25@yahoo.com>
2020-09-25 18:17:26 +0200

    setup oauth and User module

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const morgan = require('morgan');
 const hbs = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
+
+//routes
 const mainRouter = require('./routes/index');
+const authRouter = require('./routes/auth');
 
 //Load config
 dotenv.config({ path: './config/config.env' });
+
+//passport config
+require('./config/passport')(passport);
 
 connectDB();
 
@@ -22,10 +30,22 @@ if (process.env.NODE_ENV === 'development') {
 app.engine('.hbs', hbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
 
+//express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 //Static
 app.use(express.static(path.join(__dirname, 'public')));
 //Routes
 app.use('/', mainRouter);
+app.use('/auth', authRouter);
 
 const PORT = process.env.PORT || 5000;
 
